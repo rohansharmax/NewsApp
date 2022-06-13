889346e0f1df5cb74ee9cbc967bfd8025f4a2bd4
rohansharmax <rohansharma22r@gmail.com>
2022-06-14 03:05:05 +0530

    Updates

---
+++ precache-manifest.82aeddb539433f0ac6f10e4a54a3a654.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "09dbfa3beb31b8598d1f",
+    "url": "/static/css/main.d01cd86d.chunk.css"
+  },
+  {
+    "revision": "09dbfa3beb31b8598d1f",
+    "url": "/static/js/main.09dbfa3b.chunk.js"
+  },
+  {
+    "revision": "e7e0e04a5ea3842eb42e",
+    "url": "/static/js/1.e7e0e04a.chunk.js"
+  },
+  {
+    "revision": "631463bdf0b177f9342e",
+    "url": "/static/js/2.631463bd.chunk.js"
+  },
+  {
+    "revision": "6a4751f849122296e7c6",
+    "url": "/static/js/runtime~main.6a4751f8.js"
+  },
+  {
+    "revision": "49b9b8da341d1652a844a6fe93169973",
+    "url": "/index.html"
+  }
+];
